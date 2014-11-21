271f122ec33af43796741a417dcd6cb7b4eb6053
bastel <121133889+845731@users.noreply.github.com>
2014-11-21 01:25:49 +0100

    library.c: various fixes, debug, TODO: prefixes

--- c/s400w.h
+++ c/s400w.h
@@ -1,99 +1,134 @@
 #ifndef s400w_h_included
 #define s400w_h_included
 
+/* This file is licensed under Creative Commons License CC-CC0 1.0 (http://creativecommons.org/publicdomain/zero/1.0/).
+ *
+ * Created 2014-11-16 by bastel.
+ *
+ * Implements a low level interface to the Mustek(tm) S400W iScanAir(R) scanner.
+ *
+ * This is a clean room implementation based on written down specs (see commands.txt)
+ * and does not contain any code by mustek. Using simple c and non blocking I/O,
+ * it's easy to understand, clean and easily portable to other programming languages.
+ *
+ * Scanner related methods usually return a byte array containing the scanner's response. Please be aware
+ * that the response array is not a copy and not constant for unknown responses but will be altered on the next
+ * call to any scanner related method. Do not store it 'for later'.
+ * Returned known responses are mapped to their static equivalent and can be stored and compared with <code>==</code>.
+ * This also means that this class is not thread safe, but only needs a little ram (around 32 kiB).
+ * 
+ * This library is licensed under the Creative Commons License CC-CC0 1.0 (http://creativecommons.org/publicdomain/zero/1.0/).
+ *
+ * @author bastel
+ */
+
 
 /** Version */
-extern const char const* S400W_LIB_VERSION;
+extern const char* S400W_LIB_VERSION;
 
 /** Firmware version necessary to set (higher) resolution. */
 extern const int MIN_SET_RESOLUTION_FW;
 	
 // Responses (note that no response starts with another response, so no terminating character necessary)
 /** Response: Device busy */
-extern const unsigned char DEVICE_BUSY[];
+extern const char DEVICE_BUSY[];
 
 /** Response: Battery low */
-extern const unsigned char BATTERY_LOW[];
+extern const char BATTERY_LOW[];
 
 /** Response: No paper inserted */
-extern const unsigned char NOPAPER[];
+extern const char NOPAPER[];
 
 /** Response: Paper inserted, ready to scan, calibrate, clean */
-extern const unsigned char SCAN_READY[];
+extern const char SCAN_READY[];
 
 /** Response: Calibration has started */
-extern const unsigned char CALIBRATE_GO[];
+extern const char CALIBRATE_GO[];
 
 /** Response: Calibration has finished */
-extern const unsigned char CALIBRATE_END[];
+extern const char CALIBRATE_END[];
 
 /** Response: Cleaning has started */
-extern const unsigned char CLEAN_GO[];
+extern const char CLEAN_GO[];
 
 /** Response: Cleaning has finished */
-extern const unsigned char CLEAN_END[];
+extern const char CLEAN_END[];
 
 /** Response: Standard DPI selected */
-extern const unsigned char DPI_STANDARD[];
+extern const char DPI_STANDARD[];
 
 /** Response: High DPI selected */
-extern const unsigned char DPI_HIGH[];
+extern const char DPI_HIGH[];
 
 /** Response: Scanning has started */
-extern const unsigned char SCAN_GO[];
+extern const char SCAN_GO[];
 
 /** Response: Preview data in stream end marker */
-extern const unsigned char PREVIEW_END[];
+extern const char PREVIEW_END[];
 
 /** Response: JPEG size */
-extern const unsigned char JPEG_SIZE[];
+extern const char JPEG_SIZE[];
 
 /** Artifical response: EOF */
-extern const unsigned char SEOF[];
+extern const char SEOF[];
 
 /** Artifical response: EOF */
-extern const unsigned char SERR[];
+extern const char SERR[];
 
 
-extern int isKnownResponse(const unsigned char* response);
+extern int isKnownResponse(const char* response);
 
 
-/**
- * Notifies clients of preview data, end of preview data, jpeg size, jpeg data, and end of jpeg data.<br>
+/* Notifies clients of preview data, end of preview data, jpeg size, jpeg data, and end of jpeg data.
  * Note: EOF for jpeg is guaranteed to be called if jpeg size has been given (as to close file handles etc).
  * 
- * @param data either an array of bytes with preview or jpeg data, {@link S400W#EOF}
- * 		if end of preview/jpeg data, or {@link S400W#JPEG_SIZE} for jpeg size.
- * @param offset the offset inside the given array if not EOF or size.
- * @param length number of bytes to read from data or jpeg size of data is {@link S400W#JPEG_SIZE}.
- * @return <code>false</code> to abort receiving (not supported yet), <code>true</code> otherwise.
- * @throws IOException If something goes wrong while processing the data.
+ * data: either an array of bytes with preview or jpeg data, SEOF if end of preview/jpeg data,
+ *       or JPEG_SIZE for jpeg size.
+ * offset: the offset inside the given array if not SEOF or size.
+ * length: number of bytes to read from data or jpeg size of data is JPEG_SIZE.
+ *
+ * Returns <=0 to abort receiving, 1 otherwise. (negative values indicate error condition)
  */
-typedef int (*receiveFunc)(const unsigned char* data, int offset, int length);
+typedef int (*receiveFunc)(const char* data, int offset, int length);
 
 
-// Reads the scanner's version. Returns a version string, SEOF, or NULL if timeout.
-extern const unsigned char* getVersion(const char* host, int port);
+/* Reads the scanner's version. Returns a version string, SEOF, or NULL if timeout. */
+extern const char* getVersion(const char* host, int port);
 
-// Reads scanner's current status. Returns response, SEOF, or NULL if timeout.
-extern const unsigned char* getStatus(const char* host, int port);
+/* Reads scanner's current status. Returns response, SEOF, or NULL if timeout. */
+extern const char* getStatus(const char* host, int port);
 
-// Sets the scanner's resolution if supported (see MIN_SET_RESOLUTION_FW).
-// Supported DPI settings: 300 or 600.
-// Returns 1 if the resolution change was successful, 0 otherwise.
+/* Sets the scanner's resolution if supported (see MIN_SET_RESOLUTION_FW).
+ * Supported DPI settings: 300 or 600.
+ * Returns 1 if the resolution change was successful, 0 otherwise.
+ */
 extern int setResolution(const char* host, int port, int dpi);
 
 
-// Executes the scanner's cleaning routine.
-// Returns CLEAN_END if sucessfully finished, any other otherwise, 
-// including SEOF or NULL for timeouts. NOPAPER if cleaning sheet is not inserted.
-extern const unsigned char* clean(const char* host, int port);
+/* Executes the scanner's cleaning routine.
+ * Returns CLEAN_END if sucessfully finished, any other otherwise, 
+ * including SEOF or NULL for timeouts. NOPAPER if cleaning sheet is not inserted.
+ */
+extern const char* clean(const char* host, int port);
 
 
-// Executes the scanner's calibration routine.
-// Returns CALIBRATE_END if sucessfully finished, any other otherwise
-// including SEOF or NULL for timeouts. NOPAPER if calibration sheet is not inserted.
-extern const unsigned char* calibrate(const char* host, int port);
+/* Executes the scanner's calibration routine.
+ * Returns CALIBRATE_END if sucessfully finished, any other otherwise
+ * including SEOF or NULL for timeouts. NOPAPER if calibration sheet is not inserted.
+ */
+extern const char* calibrate(const char* host, int port);
 
+
+/* Executes the scanner's scanning procedure.
+ * resolution: resolution setting, or <code>0</code> if no setting is supported / desired
+ * preview: callback handler for preview data, or <code>null</code> if no preview should be read
+ * jpeg: callback handler for jpeg data
+ * Returns SCAN_READY if sucessfully finished, any other response otherwise, including SEOF or NULL for timeouts.
+ */
+extern const char* scan(const char* host, int port, int resolution, receiveFunc previewFunc, receiveFunc jpegFunc);
+
+
+/** For debugging assign printf or your own function. */
+extern int (*S400W_DEBUG)(const char* format, ...);
 
 #endif
